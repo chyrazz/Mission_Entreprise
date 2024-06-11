d7806deb8ebeb08b4d367dce5393860bcb4b1454
Yosra haboubi <yosrahaboubi27@gmail.com>
2024-06-12 00:20:45 +0100

    preparer structure template angular

---
+++ frontend_Angular/src/app/lead/lead.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lead',
+  templateUrl: './lead.component.html',
+  styleUrls: ['./lead.component.scss']
+})
+
+export class LeadComponent {
+
+}
