1f297bd574529817643bd405d079fc9b4493f7e8
Yosra haboubi <yosrahaboubi27@gmail.com>
2024-07-09 21:48:21 +0000

    spring batch config

---
+++ backend_SB/src/main/java/tn/esprit/crmassurance/utils/springBatchConfig.java
@@ -0,0 +1,9 @@
+package tn.esprit.crmassurance.utils;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class springBatchConfig {
+}
